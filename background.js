debc9fc1b56e73727eb473e22779729ccdceb8e0
shearytan <shearytan95@gmail.com>
2019-03-20 15:02:09 +1100

    Added the url tracker and the notification together

--- background.js
+++ background.js
@@ -1,22 +1,34 @@
-chrome.alarms.onAlarm.addListener(function() {
-    chrome.browserAction.setBadgeText({text: ''});
-    chrome.storage.sync.get("message", function(val) {
-        chrome.notifications.create({
-            type: 'basic',
-            title: 'Be Awesome!',
-            iconUrl:  'stay_hydrated.png',
-            message: val.message,
-            buttons: [
-                {title: 'Remind me tomorrow :)'}
-            ],
-            priority: 0
-        })
-    })
+// chrome.alarms.onAlarm.addListener(function() {
+//     chrome.browserAction.setBadgeText({text: ''});
+//     chrome.storage.sync.get("message", function(val) {
+//         chrome.notifications.create({
+//             type: 'basic',
+//             title: 'Be Awesome!',
+//             iconUrl:  'stay_hydrated.png',
+//             message: val.message,
+//             buttons: [
+//                 {title: 'Remind me tomorrow :)'}
+//             ],
+//             priority: 0
+//         })
+//     })
     
-});
+// });
 
 chrome.webNavigation.onCompleted.addListener(function(val) {
     if (val.url && val.url.match(/www\.google\.com/)) {
-        console.log("This is the google chrome: ", val)
+        chrome.browserAction.setBadgeText({text: ''});
+        chrome.storage.sync.get("message", function(val) {
+            chrome.notifications.create({
+                type: 'basic',
+                title: 'Be Awesome!',
+                iconUrl:  'stay_hydrated.png',
+                message: val.message,
+                buttons: [
+                    {title: 'Remind me tomorrow :)'}
+                ],
+                priority: 0
+            })
+        })
     }
 })
